87e51a5576524ee11784b7e265c943b586f468b6
Paul Watson <mkpw2@cox.net>
2012-08-15 22:15:23 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wartorn::Application.config.secret_token = 'ea634313cdeef7d543569e6fccca159a041b25c92a641ac8bde8af07e3fb413ca151f78b960a5dff78e5cd266a56b47a89cb6a75ea8f5c170a57b8426aef7a33'
